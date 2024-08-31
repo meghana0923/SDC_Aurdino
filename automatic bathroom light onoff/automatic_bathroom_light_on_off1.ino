dece2c0ae7098793f2a773abb4037304881f0a18
meghana0923 <meghajampani23@gmail.com>
2024-08-31 15:51:06 +0530

    First aurdino exp

---
+++ automatic bathroom light onoff/automatic_bathroom_light_on_off1.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(3,INPUT);
+  pinMode(1,OUTPUT);
+}
+
+void loop()
+{
+  int a=digitalRead(3);
+  if(a==1)
+  {
+    digitalWrite(1,HIGH);
+  }
+  else
+  {
+    digitalWrite(1,LOW);
+  }
+}
